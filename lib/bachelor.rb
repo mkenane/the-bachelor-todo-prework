5b752ce01f78798ea71eed0b080e189a893e1138
Mor Kenane <morkenan@hotmail.com>
2018-03-07 23:43:42 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,70 @@
+
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+winner = nil 
+  data.collect do |seasons, info|
+    if seasons == season
+      info.collect do |contestant|
+        if contestant["status"] == "Winner"
+          winner = contestant["name"].split[0]
+        end 
+      end 
+    end 
+  end 
+winner 
 end
 
+
+
 def get_contestant_name(data, occupation)
-  # code here
+proflady = nil 
+data.collect do |seasons, info|
+  info.collect do |contestant|
+    if contestant["occupation"] == occupation
+      proflady = contestant["name"]
+    end 
+  end
+end 
+proflady
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ counter = 0 
+  data.collect do |seasons, info|
+    info.collect do |contestants|
+      if contestants["hometown"] == hometown
+        counter += 1 
+      end 
+    end
+  end 
+counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = nil 
+  data.collect do |seasons, info|
+    cont = info.find do |contestant| 
+      contestant["hometown"] == hometown
+      end 
+    if !cont 
+    else occupation = cont["occupation"]
+    end 
+  end 
+occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages_added = 0 
+  num_of_cont = data[season].length 
+  data.each do |seasons, info|
+    if season == seasons
+      info.each do |contestant|
+         ages_added += contestant["age"].to_f
+       end
+     end
+   end 
+   puts ages_added
+   puts num_of_cont
+average = (ages_added/num_of_cont).round 
 end
