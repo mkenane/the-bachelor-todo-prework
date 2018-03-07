7898a6ebf51bea236cb307600bc531039008062a
Mor Kenane <morkenan@hotmail.com>
2018-03-07 23:43:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -60,7 +60,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasons, info|
     if season == seasons
       info.each do |contestant|
-         ages_added += contestant["age"].to_i
+         ages_added += contestant["age"].to_f
        end
      end
    end 
